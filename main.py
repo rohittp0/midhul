86b59eda739f558f730e28ad09ac407ca6cff8bb
Rohit <45072928+rohittp0@users.noreply.github.com>
2024-03-26 15:05:48 +0530

    fix

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from picamera import PiCamera
 from picamera.array import PiRGBArray
 import io
 import time
+from PIL import Image
 
-# Initialize the camera
+# initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
+camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 
-# Allow the camera to warm up
 time.sleep(2)
 
 # The URL to your Flask server endpoint
-url = 'http://<Flask_Server_IP>:8000/'
+url = 'http://192.168.0.135:8000/'
 
 print("Will talk to server at", url)
 
@@ -25,15 +26,14 @@ def send_for_prediction(image_path):
     return response
 
 # Continuously capture images from the camera and send them for processing
-for frame in camera.capture_continuous(rawCapture, format='jpeg', use_video_port=True):
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     # Save the frame as an in-memory file
-    stream = io.BytesIO(frame.array)
-    image_path = 'temp_image.jpg'  # Temporary image path
-    with open(image_path, 'wb') as f:
-        f.write(stream.getvalue())
-    
+    image = frame.array
+    Image.fromarray(image).save("temp_image.png")
+    # cv2.save("temp_image.png", image)
+  
     # Send the image for prediction
-    response = send_for_prediction(image_path)
+    response = send_for_prediction("temp_image.png")
     if response.ok:
         print(response.json())
     else:
